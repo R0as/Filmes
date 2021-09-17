1d3786f7f9ba30dcaa658668ca4c52c689f631b7
R0as <henriqueroas2002@gmail.com>
2021-09-16 21:21:19 -0300

    reunião 16/09

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -14,8 +14,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Arabic:wght@100&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="/css/reset.css">
-    <link rel="stylesheet" href="/css/styles.css">
+    <link rel="stylesheet" href={{ asset("/css/reset.css") }}>
+    <link rel="stylesheet" href={{ asset("/css/styles.css") }}>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
 
     <style>
@@ -30,10 +30,13 @@
 <body>
     <header class="header">
         <nav class="navbar">
-            <h6 class="name-site">Filmes do ben10</h6>
+            <h6 class="name-site">Filmes do ben10
+                {{-- <img src="logo.jpg" alt="omnitrix">     --}}
+            </h6>
+            
             <ul class="nav-list">
-                <li><a class="btn-nav efeito" href="{{ route('movie.index') }}">Inicio</a></li>
-                <li> <a class="btn-nav efeito" href="{{ route('movie.create') }}">Criar filme</a></li>
+                <li><a class="btn-nav efeito" href="{{ route('movie.index') }}">Início</a></li>
+                <li> <a class="btn-nav efeito" href="{{ route('movie.create') }}">novo filme</a></li>
             </ul>
         </nav>
     </header>
